51f3d4cb7a23219096f6484e385a1304fc680614
HannesMalterOffical <hannesmalter1234@gmail.com>
2022-10-05 15:05:30 +0300

    Add Razor views

--- OdeToCode/Data/ApplicationDbContext.cs
+++ OdeToCode/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using OdeToCode.Models;
 
 namespace OdeToCode.Data
 {
@@ -12,5 +13,6 @@ namespace OdeToCode.Data
             : base(options)
         {
         }
+        public DbSet<OdeToCode.Models.RestaurantReview> RestaurantReview { get; set; }
     }
 }
