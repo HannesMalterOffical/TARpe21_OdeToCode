76a09a6800f47fa801e3a8a4fe0425a680a7998e
HannesMalter <Hannesmalter1234@gmail.com>
2022-10-19 15:09:10 +0300

    Add custom validation attribute

--- OdeToCode/Models/RestaurantReview.cs
+++ OdeToCode/Models/RestaurantReview.cs
@@ -6,6 +6,28 @@ using System.Threading.Tasks;
 
 namespace OdeToCode.Models
 {
+    public class  MaxWordsAttribute:ValidationAttribute
+    {
+        private readonly int _maxWords;
+        public MaxWordsAttribute(int maxWords) : base( "{0} has too many words.") 
+        {
+            _maxWords = maxWords;
+        }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value!=null)
+            {
+                var valueAsString = value.ToString();
+                if (valueAsString.Split(' ').Length > _maxWords)
+                {
+                    var errorMessage = FormatErrorMessage(validationContext.DisplayName);
+                    return new ValidationResult(errorMessage);
+                }
+            }
+            return base.IsValid(value, validationContext);
+        }
+    }
+
     public class RestaurantReview
     {
         public int Id { get; set; }
